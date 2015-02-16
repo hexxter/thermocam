fd1ed3d49e47c80e23eae2d9bc35171394911d4d
Alexander Kratzer <a.kratzer@kratzer.it>
2015-02-16 12:46:15 +0100

    weiter mit control, uart laeuft

--- uart.py
+++ uart.py
@@ -3,7 +3,7 @@ import threading
 
 class thermo_layser(threading.Thread):
 	
-	def __init__(self, serialport="/dev/ttyUSB0", baud=115200, timeout=1):
+	def __init__(self, serialport="/dev/ttyUSB0", baud=115200, timeout=None):
 		threading.Thread.__init__(self)
 		self.serialport = serialport
 		self.baud = baud
@@ -11,11 +11,11 @@ class thermo_layser(threading.Thread):
 		self.stopped = threading.Event()
 		self.ser = serial.Serial( self.serialport, self.baud, timeout=self.timeout )
 		self.ser.open()
-		self.ser.flushInput()
 		self.res = bytes()
 		self.lock = threading.Lock()
 		self.enviroment_temp = 0.0
 		self.target_temp = 0.0
+		self.new_data = False
 
 	def write(self, txt):
 		output = str.encode(txt)
@@ -29,20 +29,42 @@ class thermo_layser(threading.Thread):
 	def led_off(self):
 		self.write('n')
 
+	def reset_new_data(self):
+		self.lock.acquire()
+		self.new_data = False
+		self.lock.release()
+
+	def is_new_data(self):
+		return self.new_data
+
 	def _parseline(self):
-		#line = self.res.decode().replace( '\\x00','' )
-		print( self.res.decode() )
+		line = self.res.decode().replace( '\r', '' )
+		data = line.split( ";" )
+		self.lock.acquire()
+		try:
+			self.target_temp = float(data[1])
+			self.enviroment_temp = float(data[2])
+			self.new_data = True
+		except ValueError as e:
+			self.new_data = False
+			print( e )
+			
+		self.lock.release()
 
 	def run(self):
 		self.ser.flushInput()
+		firstinput = True
 		while not self.stopped.wait(0.001):
-			if self.ser.inWaiting() > 0:
-				income = self.ser.read(1)
-				if income.decode() == '\n':
-					self._parseline()
+			income = self.ser.read()
+			if '\n' in income.decode():
+				if firstinput:
+					firstinput = False
 					self.res = bytes()
 				else:
-					self.res += income
+					self._parseline()
+					self.res = bytes()
+			else:
+				self.res += income
 		
 		self.ser.close()
 						
@@ -57,9 +79,16 @@ if  __name__ =='__main__':
 
 	thermo1 = thermo_layser()
 	thermo1.start()
-	
+
+	oldtime = time.time()	
+
 	thermo1.led_on()
-	time.sleep( 60*60*5 )
+	for i in range( 10000 ):
+		if thermo1.is_new_data():
+			thermo1.reset_new_data()
+			print( "target: %f env: %f time delta: %f" % (thermo1.target_temp, thermo1.enviroment_temp, time.time()-oldtime) )
+			oldtime = time.time()
+		time.sleep( .002 )
 	thermo1.led_off()
 	
 	thermo1.stop()
